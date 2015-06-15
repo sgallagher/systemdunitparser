87e477bd1ee297547e3dc33f26f54c63895fe410
Stephen Gallagher <sgallagh@redhat.com>
2015-06-15 12:49:29 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='systemdunitparser',
+    version='0.1',
+    packages=['SystemdUnitParser'],
+    url='http://github.com/sgallagh/systemdunitparser',
+    license='GPLv3+',
+    author='Stephen Gallagher',
+    author_email='sgallagh@redhat.com',
+    description='Parser to read and create unit files for systemd'
+)
