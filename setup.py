79ed49930cb87ac0d691c3ecb0e3bb481806bb03
Konrad Weihmann <kweihmann@outlook.com>
2022-03-02 11:20:13 +0100

    setup.py: add more metadata
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
 
+with open("README.md") as i:
+    _long_description = i.read()
+
 setup(
     name='systemdunitparser',
-    version='0.2',
+    author_email='sgallagh@redhat.com',
+    author='Stephen Gallagher',
+    description='Parser to read and create unit files for systemd',
+    license='GPL-3.0-or-later',
+    long_description=_long_description,
+    long_description_content_type="text/markdown",
     packages=['SystemdUnitParser'],
     url='http://github.com/sgallagh/systemdunitparser',
-    license='GPL-3.0-or-later',
-    author='Stephen Gallagher',
-    author_email='sgallagh@redhat.com',
-    description='Parser to read and create unit files for systemd'
+    version='0.2',
+    license_files=('LICENSE',),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
 )
